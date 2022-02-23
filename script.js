9271a1e87f4678b3f8b40daf6b33e7ed1de57e68
Sebastian Solis <solisfenger@gmail.com>
2022-02-23 11:51:44 -0300

    added the nav-end no the dynamic function in Javascript

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const menu = document.querySelector("#menu");
 const navcenter = document.querySelector(".nav-center");
+const navend = document.querySelector(".nav-end");
 
 menu.addEventListener("click", () => {
     menu.classList.toggle("active")
     navcenter.classList.toggle("active")
+    navend.classList.toggle("active")
 })
